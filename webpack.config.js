f4c07f288cc9aed66e54329b87e854c67aed407d
Ian Conrad <ijcon40@gmail.com>
2019-06-21 09:35:52 -0400

    updated to include jsx interpretation

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+
     ]
   },
   plugins: [htmlPlugin]
