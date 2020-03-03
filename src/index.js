7e7636c5f349bbd9d5f49b01a03a4f092ca98563
Ian Conrad <ijcon40@gmail.com>
2020-03-03 11:30:45 -0500

    added some basic files for d3 testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
 const Index = () => {
-	return <div>Hello, World!</div>;
+	return App();
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
